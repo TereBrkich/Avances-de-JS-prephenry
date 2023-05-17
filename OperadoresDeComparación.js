50187de022d345b3b7d137bbc50debc6f1a4672c
TereBrkich <teresabrkich@gmail.com>
2023-05-17 16:10:49 -0300

    Avances de JS Curso Henry

---
+++ OperadoresDeComparación.js
@@ -0,0 +1,26 @@
+//Operaciones de comparacion
+console.log(4<7);
+console.log(4<1);
+console.log(4>4);
+console.log(4==7);
+
+
+//Igualdad vs igualdad estricta
+console.log(3==3);
+console.log(3===3);
+console.log(3=='3');
+console.log(3==='3');
+console.log(7=='7');
+console.log(7==='7')
+
+
+//Asignacion y asociatividad 
+var a =1;
+var b = 2;
+var c = (a=b);
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+console.log(16/2/4);
---
+++ Variables.js
@@ -0,0 +1,20 @@
+
+//String
+var bootcamp ='Henry';
+
+//Numeros
+var numeroEntero = 14;
+var numeroNegativo= -4;
+var nuemroDecimal = 11.75;
+
+//Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+var cajon1;
+console.log (cajon1);
+
+// Null 
+var sinNada = null;
+
